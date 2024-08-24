abe21c91e943ca7b3b803a793910662066c9ba7e
Ahmed_Adel <109729603+ahmeda335@users.noreply.github.com>
2024-08-24 17:39:35 +0300

    Update Merges.py

--- llm_merging/merging/Merges.py
+++ llm_merging/merging/Merges.py
@@ -51,7 +51,7 @@ class Merges(object):
     def _load_base_model(self):
         bnb_config = BitsAndBytesConfig(load_in_8bit=True, 
                                        llm_int8_enable_fp32_cpu_offload=True,
-                                       llm_int3_skip_modules=["lm_head"]) #, bnb_4bit_use_double_quant=True,
+                                       llm_int8_skip_modules=["lm_head"]) #, bnb_4bit_use_double_quant=True,
                                                     # bnb_4bit_quant_type="nf4",
                                                      #bnb_4bit_compute_dtype=torch.bfloat16,
                                                      #)
