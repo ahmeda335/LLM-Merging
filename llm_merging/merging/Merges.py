f29bb2a82c9fcedbf36def677eaee68f3383c685
Ahmed_Adel <109729603+ahmeda335@users.noreply.github.com>
2024-08-24 17:17:04 +0300

    Removing quantization from main branch

--- llm_merging/merging/Merges.py
+++ llm_merging/merging/Merges.py
@@ -49,13 +49,10 @@ class Merges(object):
         raise NotImplementedError
 
     def _load_base_model(self):
-        bnb_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_use_double_quant=True,
-                                                     bnb_4bit_quant_type="nf4",
-                                                     bnb_4bit_compute_dtype=torch.bfloat16)
         if self.architecture == "encoder_decoder":
-            self.base_model =  AutoModelForSeq2SeqLM.from_pretrained(self.base_model_name, revision=self.base_model_revision_id, quantization_config=bnb_config, token=os.environ["HF_AUTH_TOKEN"], device_map='auto')#.to(self.device)
+            self.base_model =  AutoModelForSeq2SeqLM.from_pretrained(self.base_model_name, revision=self.base_model_revision_id, token=os.environ["HF_AUTH_TOKEN"]).to(self.device)
         elif self.architecture == "decoder":
-            self.base_model =  AutoModelForCausalLM.from_pretrained(self.base_model_name, revision=self.base_model_revision_id, quantization_config=bnb_config, token=os.environ["HF_AUTH_TOKEN"], device_map='auto')#.to(self.device)
+            self.base_model =  AutoModelForCausalLM.from_pretrained(self.base_model_name, revision=self.base_model_revision_id, token=os.environ["HF_AUTH_TOKEN"]).to(self.device)
         else:
             raise NotImplementedError(f"Architecture not implemented {self.architecture}")
         
